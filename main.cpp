ffe65296ba760a9b8dc1993db1eb7cb5dc053ae5
Felix Godziela <joemano.elcooll@gmail.com>
2021-10-11 14:57:32 -0300

    Added player

--- main.cpp
+++ main.cpp
@@ -4,10 +4,69 @@
 
 using namespace std;
 
+
+
+//Player.cpp
+//--------------------------------------------------------------------------------------------------------------------------------------
+class Player
+{
+private:
+	Vector2 _pos;
+
+public:
+	Player();
+	~Player();
+	void set_pos_x(int x);
+	void set_pos_y(int y);
+	Vector2 get_pos();
+};
+Player::Player()
+{
+	_pos = {0,0};
+}
+Player::~Player()
+{
+	cout << "Bye bye" << endl;
+}
+void Player::set_pos_x(int x)
+{
+	_pos.x = x;
+}
+void Player::set_pos_y(int y)
+{
+	_pos.y = y;
+}
+Vector2 Player::get_pos()
+{
+	return _pos;
+}
+//-----------------------------------------------------------------------------------------------
+
+
+void init_game()
+{
+	Player* player = new Player();
+}
+
+
+
+
+
+
+
+
+
+
+
+void play()
+{
+	InitWindow(800, 450, "Flappy Bord");
+	init_game();
+}
+
 void main()
 {
-
-
+	play();
 	fflush(stdin);
 	cin.get();
 }
